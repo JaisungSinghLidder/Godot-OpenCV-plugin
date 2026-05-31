d0c5d3f32006990ac558f4ab5bc80e48340b1bda
JaisungSinghLidder <jaisung.lidder@gmail.com>
2026-05-31 03:42:50 -0700

    Did some c++ messing about

---
+++ src/test.cpp
@@ -0,0 +1,31 @@
+#include "test.hpp"
+
+#include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/core/object.hpp"
+#include "godot_cpp/core/print_string.hpp"
+
+using namespace godot; 
+
+
+void Test::say_hello()
+{
+    print_line("Helloo");
+}
+
+
+void Test::_bind_methods()
+{
+    ClassDB::bind_method(D_METHOD("say_hello"), &Test::say_hello);
+    ClassDB::bind_method(D_METHOD("get_my_data"), &Test::get_my_data);
+    ClassDB::bind_method(D_METHOD("set_my_data", "new_data"), &Test::set_my_data);
+}
+
+String Test::get_my_data() const
+{
+    return my_data; 
+}
+
+void Test::set_my_data(const String &new_data)
+{
+    my_data = new_data 
+}
---
+++ src/test.hpp
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include "godot_cpp/classes/node.hpp"
+#include "godot_cpp/classes/wrapped.hpp"
+#include "godot_cpp/variant/string.hpp"
+
+
+class Test : public godot::Node {
+    //class, inherited class 
+    GDCLASS(Test, Node)
+protected: 
+    static void _bind_methods();
+private:
+    String my_data = "Subscribe me";
+
+    String get_my_data() const; 
+
+    void set_my_data(const String &new_data);
+    void say_hello();
+    // code 
+};
