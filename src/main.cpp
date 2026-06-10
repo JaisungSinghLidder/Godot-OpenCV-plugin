f894f496df41f929745ad37905743099d2c77e7f
JaisungSinghLidder <jaisung.lidder@gmail.com>
2026-06-10 00:04:45 -0700

    new testing verison

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "main.hpp"
+
+
+Main::Main() {}
+Main::~Main() {}
+
+
+void Main::_bind_methods() {
+
+}
--- src/main.hpp
+++ src/main.hpp
@@ -1,3 +1,15 @@
-class Main {
-    
+#pragma once
+
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+class Main : public Node {
+    GDCLASS(Main, Node)
+
+private:
+    Main();
+    ~Main();
+    static void _bind_methods();
 };
